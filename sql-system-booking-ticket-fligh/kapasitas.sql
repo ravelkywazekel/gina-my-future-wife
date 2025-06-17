7c18ef10342cc8e437cd48d606c7b15cb2c6f941
Ravelky Wazekel <59724023+ravelkywazekel@users.noreply.github.com>
2025-06-17 22:47:58 +0700

    Create kapasitas.sql

---
+++ sql-system-booking-ticket-fligh/kapasitas.sql
@@ -0,0 +1,56 @@
+-- CREATE: table
+CREATE TABLE kapasitas (
+    id_pesawat VARCHAR(10) PRIMARY KEY,
+    id_kursi VARCHAR(10) NOT NULL,
+    kapasitas_kursi INTEGER NOT NULL
+);
+
+
+
+
+
+
+-- INSERT: data
+INSERT INTO kapasitas (id_pesawat, id_kursi, kapasitas) VALUES
+('PE001', '45A', 780),
+('PE002', '07A', 8),
+('PE003', '50A', 770),
+('PE004', '53D', 750),
+('PE005', '42E', 60);
+
+  --  SELECT: awalnya data
+SELECT * FROM kapasitas
+
+
+
+
+  
+
+-- UPDATE: ubah kapasitas untuk penerbangan PE002
+UPDATE kapasitas
+SET kapasitas_kursi = 15
+WHERE id_pesawat = 'PE002';
+
+  --  SELECT: setelah di ubah
+SELECT * FROM kapasitas
+
+
+
+
+
+
+-- DELETE: hapus catatan kapasitas untuk penerbangan PE005
+DELETE FROM kapasitas
+WHERE id_pesawat = 'PE005';
+
+  --  SELECT: setelah di hapus
+SELECT * FROM kapasitas
+
+
+
+
+
+    
+-- SELECT: menampilkan kapasitas kursi jika kapasitasnya lebih besar dari 100
+SELECT * FROM kapasitas
+WHERE kapasitas_kursi > 100;
