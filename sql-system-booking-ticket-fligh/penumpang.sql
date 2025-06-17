35d5d9c3eabfc7771f111e6f855e4666da2013ce
Ravelky Wazekel <59724023+ravelkywazekel@users.noreply.github.com>
2025-06-17 21:48:16 +0700

    Update penumpang.sql

--- sql-system-booking-ticket-fligh/penumpang.sql
+++ sql-system-booking-ticket-fligh/penumpang.sql
@@ -1,1 +1,44 @@
-test
+-- CREATE: table
+CREATE TABLE penumpang (
+    id_penumpang VARCHAR(10) PRIMARY KEY,
+    nama VARCHAR(100) NOT NULL,
+    jenis_kelamin VARCHAR(10) CHECK (jenis_kelamin IN ('PRIA', 'WANITA'))
+);
+
+
+
+-- INSERT: data
+INSERT INTO pemumpang (id_penumpang, nama, jenis_kelamin) VALUES 
+('P001', 'DINAR DANISH', 'WANITA'),
+('P002', 'NILAM SARI', 'WANITA'),
+('P003', 'ABI SYAHLI', 'PRIA'),
+('P004', 'ZENDAYA', 'WANITA'),
+('P005', 'ABU LAHAB', 'PRIA');
+
+  --  SELECT: awalnya data
+SELECT * FROM penumpang
+
+
+
+
+  
+
+-- UPDATE: ubah jenis kelamin penumpang P004
+UPDATE penumpang
+SET jenis_kelamin = 'PRIA'
+WHERE id_penumpang = 'P004';
+
+  --  SELECT: setelah di ubah
+SELECT * FROM penumpang
+
+
+
+
+
+
+-- DELETE: hapus penumpang P005
+DELETE FROM penumpang
+WHERE id penumpang = 'P005';
+
+  --  SELECT: setelah di hapus
+SELECT * FROM penumpang
