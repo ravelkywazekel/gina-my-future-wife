b94f63c33b193bbdb0461d9333d5e2f58c34c885
Ravelky Wazekel <59724023+ravelkywazekel@users.noreply.github.com>
2025-06-22 20:44:30 +0700

    Update operator.sql

--- sql-system-booking-ticket-fligh/operator.sql
+++ sql-system-booking-ticket-fligh/operator.sql
@@ -1,11 +1,27 @@
+/*
+-- CREATE DATABASE
+CREATE DATABASE pemesanan_tiket_pesawat;
+
+-- BACKUP DATABASE 
+BACKUP DATABASE pemesanan_tiket_pesawat
+TO DISK = 'D:\backups\pemesanan_tiket_pesawat.bak'
+WITH DIFFERENTIAL; -- menyimpan semua perubahan data yang dibuat sejak pencadangan penuh terakhir
+
+-- DROP DATABASE
+DROP DATABASE pemesanan_tiket_pesawat;
+*/
+
+
+
+
+
+
 -- CREATE: table
 CREATE TABLE operator (
     id_operator CHAR(3) UNIQUE PRIMARY KEY,
     email VARCHAR(30) NOT NULL,
     nama_operator VARCHAR(30) NOT NULL
 );
-
-
 
 
 -- INSERT: data
@@ -15,17 +31,13 @@ INSERT INTO operator (id_operator, email, nama_operator) VALUES
 ('OP3', 'KONTAK@CITILINK.CO.ID', 'CITILINK'),
 ('OP4', 'KONTAK@LIONAIR.CO.ID', 'LION AIR'),
 ('OP5', 'KONTAK@GARUDA-INDONESIA.COM', 'GARUDA INDONESIA');
-
     --  SELECT: awalnya data
     SELECT * FROM operator;
-
-
 
 
 -- ALTER: menambah kolom tabel
 ALTER TABLE operator
 ADD medsos VARCHAR(20) default 'test';
-
     -- SELECT: menambah kolom
     SELECT * FROM operator;
 
@@ -33,7 +45,6 @@ ADD medsos VARCHAR(20) default 'test';
 -- ALTER: mengubah nama kolomtabel
 ALTER TABLE operator
 CHANGE medsos media_sosial VARCHAR(20) default 'test';
-
     -- SELECT: mengubah kolom
     SELECT * FROM operator;
 
@@ -41,11 +52,8 @@ CHANGE medsos media_sosial VARCHAR(20) default 'test';
 --  ALTER: menghapus kolom tabel
 ALTER TABLE operator
 DROP COLUMN media_sosial;
-
     -- SELECT: menambah kolom
     SELECT * FROM operator;
-
-
 
 
 -- DROP: menghapus tabel
@@ -56,27 +64,47 @@ DROP COLUMN media_sosial;
 -- TRUNCATE TABLE operator;
 
 
-  
-
 -- UPDATE: ubah email untuk operator OP2
 UPDATE operator
 SET email = 'KONTAK@LIONAIR-INDONESIA.CO.ID'
 WHERE id_operator = 'OP2';
-
     --  SELECT: setelah di ubah
     SELECT * FROM operator;
-
-
-
-
 
 
 -- DELETE: hapus operator dengan operator_id OP5
 DELETE FROM operator
 WHERE id_operator = 'OP5';
-
     --  SELECT: setelah di hapus
     SELECT * FROM operator;
+
+
+/*
+-- CREATE PROCEDURE
+CREATE PROCEDURE semua_operator
+AS
+SELECT * FROM operator
+GO;
+    -- EXEC: mengeksekusi prosedur tersebut 
+    EXEC semua_operator;
+
+
+-- CREATE INDEX
+CREATE INDEX idx_nama_operator
+ON operator(nama_operator);
+    -- DROP INDEX: menghapus index tersebut
+    ALTER TABLE operator
+    DROP INDEX idx_nama_operator;
+
+
+-- CREATE VIEW
+CREATE VIEW [garuda indonesia] AS
+SELECT * FROM operator
+WHERE nama_operator = 'GARUDA INDONESIA';
+    -- SELECT: menampilkan prosedur tersebut
+    SELECT * FROM [garuda indonesia];
+*/
+
 
 
 
@@ -94,6 +122,7 @@ SELECT * FROM operator
 WHERE NOT email LIKE '%@LIONAIR.CO.ID';
 
 
+
 -- 2# operator NULL (IS NULL, IS NOT NULL)
 SELECT * FROM operator 
 WHERE email IS NULL;
@@ -102,14 +131,17 @@ SELECT * FROM operator
 WHERE email IS NOT NULL;
 
 
+
 -- 3# operator pencocokan pola (LIKE)
 SELECT * FROM operator 
 WHERE email LIKE '%@GARUDA-INDONESIA.COM';
 
 
+
 -- 4# operator set (IN, BETWEEN)
 SELECT * FROM operator 
 WHERE id_operator IN ('OP1', 'OP2');
+
 
 
 -- 5# operator query (UNION, UNION ALL, EXISTS, ANY, ALL)
@@ -146,6 +178,7 @@ SELECT * FROM operator
 WHERE id_operator = ANY (SELECT id_operator FROM backup_operator);
 
 
+
 -- 6# Sorting and grouping operators (ORDER BY, GROUP BY, HAVING)
 -- ORDER BY
 SELECT * FROM operator
@@ -156,6 +189,7 @@ SELECT email, COUNT(*) as operator_count
 FROM operator
 GROUP BY email
 HAVING operator_count > 1; 
+
 
 
 -- 7# operator lainnya (AS, CASE, DISTINCT, INTO, LIMIT)
@@ -182,6 +216,7 @@ SELECT * FROM operator;
 SELECT * FROM operator LIMIT 2;
 
 
+
 -- 8# operator function (MIN(), MAX(), COUNT(), IF() IFNULL(), COALESCE ())
 -- MIN()
 SELECT MIN(id_operator) AS min_operator_id FROM operator;
@@ -206,6 +241,7 @@ FROM operator;
 SELECT id_operator, 
        COALESCE(email, 'No Email', 'Unknown') AS email_address 
 FROM operator;
+
 
 
 -- 9# operator JOIN (INNER JOIN, RIGHT JOIN, LEFT JOIN, FULL JOIN)
