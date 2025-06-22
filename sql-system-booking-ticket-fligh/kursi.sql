1c812dce69be68263f5a024bac5715832ece0042
Ravelky Wazekel <59724023+ravelkywazekel@users.noreply.github.com>
2025-06-22 22:30:15 +0700

    Update kursi.sql

--- sql-system-booking-ticket-fligh/kursi.sql
+++ sql-system-booking-ticket-fligh/kursi.sql
@@ -1,3 +1,19 @@
+/*
+-- CREATE DATABASE
+CREATE DATABASE pemesanan_tiket_pesawat;
+
+-- BACKUP DATABASE 
+BACKUP DATABASE pemesanan_tiket_pesawat
+TO DISK = 'D:\backups\pemesanan_tiket_pesawat.bak'
+WITH DIFFERENTIAL; -- menyimpan semua perubahan data yang dibuat sejak pencadangan penuh terakhir
+
+-- DROP DATABASE
+DROP DATABASE pemesanan_tiket_pesawat;
+*/
+
+
+
+
 -- CREATE: table
 CREATE TABLE kursi (
     id_kursi CHAR(3) UNIQUE PRIMARY KEY,
@@ -8,10 +24,6 @@ CREATE TABLE kursi (
 );
 
 
-
-
-
-
 -- INSERT: data
 INSERT INTO kursi (id_kursi, class, availability, id_operator, pembayaran) VALUES
 ('45A', 'EKONOMI', 'TERSEDIA', 'OP1', 'DEBIT'),
@@ -19,40 +31,51 @@ INSERT INTO kursi (id_kursi, class, availability, id_operator, pembayaran) VALUE
 ('50A', 'EKONOMI', 'TERSEDIA', 'OP3', 'CREDIT'),
 ('53D', 'EKONOMI', 'TERSEDIA', 'OP4', 'DEBIT'),
 ('42E', 'BISNIS', 'TERISI', 'OP5', 'CASH');
-
-  --  SELECT: awalnya data
-SELECT * FROM kursi;
-
+    --  SELECT: awalnya data
+    SELECT * FROM kursi;  
 
 
+-- ALTER: menambah kolom tabel
+ALTER TABLE kursi
+ADD posisi_kursi SET('DEPAN', 'TENGAH', 'BELAKANG') default 'TENGAH';
+    -- SELECT: menambah kolom
+    SELECT * FROM kursi;
 
-  
+
+-- ALTER: mengubah nama kolomtabel
+ALTER TABLE kursi
+CHANGE posisi_kursi tempat_kursi SET('DEPAN', 'TENGAH', 'BELAKANG') default 'TENGAH';
+    -- SELECT: mengubah kolom
+    SELECT * FROM kursi;
+
+
+--  ALTER: menghapus kolom tabel
+ALTER TABLE kursi
+DROP COLUMN tempat_kursi;
+    -- SELECT: menambah kolom
+    SELECT * FROM kursi;
+
+
+/*
+-- DROP: menghapus tabel
+DROP TABLE kursi;
+
+
+-- TRUNCATE: menghapus isi data tabel
+TRUNCATE TABLE kursi;
+*/
+
 
 -- UPDATE: tandai kursi 07A sebagai TERISI
 UPDATE kursi
 SET availability = 'TERISI'
 WHERE id_kursi = '07A';
-
-  --  SELECT: setelah di ubah
-SELECT * FROM kursi;
-
-
-
-
+    --  SELECT: setelah di ubah
+    SELECT * FROM kursi;
 
 
 -- DELETE: hapus kursi 53D
 DELETE FROM kursi
 WHERE id_kursi = '53D';
-
-  --  SELECT: setelah di hapus
-SELECT * FROM kursi;
-
-
-
-
-
-    
--- SELECT: menampilkan semua kursi dengan kelas BISNIS atau FIRST CLASS
-SELECT * FROM kursi
-WHERE class IN ('BISNIS', 'FIRST CLASS');
+    --  SELECT: setelah di hapus
+    SELECT * FROM kursi;
