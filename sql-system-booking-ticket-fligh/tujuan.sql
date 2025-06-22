cac34eb3b5f0620434ffe8c8329f4b8adc7bb182
Ravelky Wazekel <59724023+ravelkywazekel@users.noreply.github.com>
2025-06-23 03:09:03 +0700

    Update tujuan.sql

--- sql-system-booking-ticket-fligh/tujuan.sql
+++ sql-system-booking-ticket-fligh/tujuan.sql
@@ -16,7 +16,7 @@ DROP DATABASE pemesanan_tiket_pesawat;
 
 -- CREATE: table
 CREATE TABLE tujuan (
-    id_pesawat VARCHAR(5) UNIQUE PRIMARY KEY AUTO_INCREMENT,
+    id_pesawat VARCHAR(5) UNIQUE PRIMARY KEY,
     id_penumpang VARCHAR(4) UNIQUE,
     tujuan_pesawat VARCHAR(15) NOT NULL
 );
@@ -30,25 +30,25 @@ INSERT INTO tujuan (id_pesawat, id_penumpang, tujuan_pesawat) VALUES
 ('PE004', 'P004', 'PADANG'),
 ('PE005', 'P005', 'PEKANBARU');
     --  SELECT: awalnya data
-    SELECT * FROM tujuan
+    SELECT * FROM tujuan;
 
         
 -- ALTER: menambah kolom tabel
-ALTER TABLE tujuan
+ALTER TABLE tujuan 
 ADD transit VARCHAR(15) default 'test';
     -- SELECT: menambah kolom
     SELECT * FROM tujuan;
 
 
 -- ALTER: mengubah nama kolomtabel
-ALTER TABLE tujuan
+ALTER TABLE tujuan 
 CHANGE transit tempat_transit VARCHAR(15) default 'test';
     -- SELECT: mengubah kolom
     SELECT * FROM tujuan;
 
 
 --  ALTER: menghapus kolom tabel
-ALTER TABLE tujuan
+ALTER TABLE tujuan 
 DROP COLUMN tempat_transit;
     -- SELECT: menambah kolom
     SELECT * FROM tujuan;
@@ -69,14 +69,14 @@ UPDATE tujuan
 SET tujuan_pesawat = 'MALANG'
 WHERE id_pesawat = 'PE003' AND id_penumpang = 'P003';
     --  SELECT: setelah di ubah
-    SELECT * FROM tujuan
+    SELECT * FROM tujuan;
 
         
 -- DELETE: hapus catatan tujuan untuk penumpang P005 pada penerbangan PE005
 DELETE FROM tujuan
 WHERE id_pesawat = 'PE005' AND id_penumpang = 'P005';
     --  SELECT: setelah di hapus
-    SELECT * FROM tujuan
+    SELECT * FROM tujuan;
 
 
 /*
@@ -99,9 +99,9 @@ ON tujuan(id_penumpang);
 
 
 -- CREATE VIEW
-CREATE VIEW garuda_destinasi AS
+CREATE VIEW tujuan_destinasi AS
 SELECT * FROM tujuan
-WHERE tujuan_pesawat LIKE 'P%';
+WHERE tujuan_pesawat = 'PADANG';
     -- SELECT: menampilkan prosedur tersebut
     SELECT * FROM tujuan_destinasi;
 
